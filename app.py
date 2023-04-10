1e7b0e41118840dd883dc1c2d4a333a4cb2d16f3
robertcunningham <robertcunningham@ford.com>
2023-04-10 13:04:45 -0700

    idk

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 openai.organization = os.getenv("OPENAI_API_ORG")
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -21,17 +21,28 @@ def exchange(messages: list[dict[str, str]]) -> str:
         temperature=0.8
     )
 
-    message = response.choices[0].text.strip()
+    message = response.choices[0].message.content
     return message
 
 
-@app.route("/chat", methods=["POST"])
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
+@app.route("/chat", methods=["POST", "OPTIONS"])
+@cross_origin()
 def chat():
     data = request.get_json(force=True)
     messages = data.get("messages")
-
+    print(messages)
     if not messages:
-        return jsonify({"error": "Please provide a messages array"}), 400
+        resp = jsonify({"error": "Please provide a messages array"})
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 400
 
     try:
         response_text = exchange(messages)
@@ -42,4 +53,3 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
